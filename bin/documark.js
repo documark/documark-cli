a35d65c0e9d4e243c1d940286d17bd4cf8bc14cd
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-02-21 22:09:30 +0100

    Move commands to local documark package.

--- bin/documark.js
+++ bin/documark.js
@@ -1,17 +1,40 @@
 #!/usr/bin/env node
 
-/* jslint node: true */
-
 'use strict';
 
-var program = require('commander');
+process.title = 'documark';
 
-program
-	.version(require('../package.json').version)
-	.command('compile', 'compile document to PDF')
-	.parse(process.argv)
-	;
+var path    = require('path');
+var nopt    = require('nopt');
+var resolve = require('resolve').sync;
+var docRoot = process.cwd();
+var parsed, documarkPath;
 
-if ( ! program.runningCommand) {
-	program.help();
+// Determine document root
+parsed = nopt({
+	'file': path
+}, {
+	'f': ['--file']
+}, process.argv, 2);
+
+if (parsed.file) {
+	docRoot = path.dirname(parsed.file);
 }
+
+try {
+	documarkPath = resolve('documark', { basedir: docRoot });
+} catch (ex) {
+	var pkg = require('../package.json');
+	[
+		'Unable to find local Documark package.',
+		'',
+		'To install it, run "npm i documark --save" in your document root:',
+		docRoot,
+		'',
+		'Need help? Go to: ' + pkg.homepage
+	].forEach(function (s) { console.error(s); });
+	process.exit(1);
+}
+
+// Require local documark and run it
+require(documarkPath).cli();
